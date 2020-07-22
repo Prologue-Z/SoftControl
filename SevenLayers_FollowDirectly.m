4b3585cc9c1780c00ad95e4061b39af7e170448f
Prologue <tjuzx2016@126.com>
2020-07-22 13:39:12 +0800

    更改文件名称

--- SevenLayers_FollowDirectly.m
+++ SevenLayers_FollowDirectly.m
@@ -6,7 +6,7 @@ clear;
 
 % 设置时间步长
 T = 5;%总时间
-delta_t = 0.000001;
+delta_t = 0.0001;
 t = 0:delta_t:5;
 
 % 部分参数
@@ -263,7 +263,7 @@ for i=1:length(t)-1
 
     D1_2(:,i) = Ret_TopThree(T23 * [D1_3(:,i);1]);
     D2_2(:,i) = Ret_TopThree(T23 * [D2_3(:,i);1]);
-    D3_2(:,i) = Ret_TopThree(T23 * [D2_3(:,i);1]);
+    D3_2(:,i) = Ret_TopThree(T23 * [D3_3(:,i);1]);
 
     D1_0(:,i) = Ret_TopThree(T01 * T12 * [D1_2(:,i);1]);
     D2_0(:,i) = Ret_TopThree(T01 * T12 * [D2_2(:,i);1]);
@@ -272,7 +272,7 @@ for i=1:length(t)-1
 
     E1_3(:,i) = Ret_TopThree(T34 * [E1_4(:,i);1]);
     E2_3(:,i) = Ret_TopThree(T34 * [E2_4(:,i);1]);
-    E2_3(:,i) = Ret_TopThree(T34 * [E2_4(:,i);1]);
+    E3_3(:,i) = Ret_TopThree(T34 * [E3_4(:,i);1]);
 
     E1_0(:,i) = Ret_TopThree(T01 * T12 * T23 * [E1_3(:,i);1]);
     E2_0(:,i) = Ret_TopThree(T01 * T12 * T23 * [E2_3(:,i);1]);
@@ -464,94 +464,162 @@ for i=1:length(t)-1
 end
 
 
-% % 绘制预期末端与实际末端轨迹plot图像
-% 直线末端轨迹
-plot3(Path_actual(1,:),Path_actual(2,:),Path_actual(3,:),'r','LineWidth',2); 
-hold on;
-% 期望末端轨迹
-plot3(Path_desired(1,:),Path_desired(2,:),Path_desired(3,:),'b','LineWidth',2);
-hold on;
-% 给定坐标范围
-axis([-0.5 0.5 -0.3 0.3 0.82 0.88]);
-grid on;%打开网格
-h=legend('实际轨迹','期望轨迹');
-xlabel('X/m');ylabel('Y/m');zlabel('Z/m'); 
-title('运动轨迹跟踪结果');
-set(h,'Fontsize',16);
-set(gca,'Fontsize',13)
-view(-60,40);
+% % % 绘制预期末端与实际末端轨迹plot图像
+% % 直线末端轨迹
+% plot3(Path_actual(1,:),Path_actual(2,:),Path_actual(3,:),'r','LineWidth',2); 
+% hold on;
+% % 期望末端轨迹
+% plot3(Path_desired(1,:),Path_desired(2,:),Path_desired(3,:),'b','LineWidth',2);
+% hold on;
+% % 给定坐标范围
+% axis([-0.5 0.5 -0.3 0.3 0.82 0.88]);
+% grid on;%打开网格
+% h=legend('实际轨迹','期望轨迹');
+% xlabel('X/m');ylabel('Y/m');zlabel('Z/m'); 
+% title('运动轨迹跟踪结果');
+% set(h,'Fontsize',16);
+% set(gca,'Fontsize',13)
+% view(-60,40);
 
-% % % 绘制并联平台仿真动画
-% % 创建视频
-% V_Sim = VideoWriter('Video_Sim.avi');
-% V_Sim.FrameRate = fix(length(t)/5);%每秒帧数
+% % 绘制并联平台仿真动画
+% 创建视频
+V_Sim = VideoWriter('Video_Sim.avi');
+V_Sim.FrameRate = fix(length(t)/5);%每秒帧数
 
-% open(V_Sim);
+open(V_Sim);
 
-% for k = 1:length(t)
-%    plot3([A1_0(1,k),A2_0(1,k)],[A1_0(2,k),A2_0(2,k)],[A1_0(3,k),A2_0(3,k)],'k','LineWidth',2); %A1-A2
-%    hold on 
-%    plot3([A1_0(1,k),A3_0(1,k)],[A1_0(2,k),A3_0(2,k)],[A1_0(3,k),A3_0(3,k)],'k','LineWidth',2); %A1-A3
-%    hold on 
-%    plot3([A2_0(1,k),A3_0(1,k)],[A2_0(2,k),A3_0(2,k)],[A2_0(3,k),A3_0(3,k)],'k','LineWidth',2); %A2-A3
-%    hold on 
+for k = 1:length(t)
 
-%    plot3([B1_0(1,k),B2_0(1,k)],[B1_0(2,k),B2_0(2,k)],[B1_0(3,k),B2_0(3,k)],'k','LineWidth',2); %B1-B2
-%    hold on 
-%    plot3([B1_0(1,k),B3_0(1,k)],[B1_0(2,k),B3_0(2,k)],[B1_0(3,k),B3_0(3,k)],'k','LineWidth',2); %B1-B3
-%    hold on 
-%    plot3([B2_0(1,k),B3_0(1,k)],[B2_0(2,k),B3_0(2,k)],[B2_0(3,k),B3_0(3,k)],'k','LineWidth',2); %B2-B3
-%    hold on 
-
-%    plot3([C1_0(1,k),C2_0(1,k)],[C1_0(2,k),C2_0(2,k)],[C1_0(3,k),C2_0(3,k)],'k','LineWidth',2); %C1-C2
-%    hold on 
-%    plot3([C1_0(1,k),C3_0(1,k)],[C1_0(2,k),C3_0(2,k)],[C1_0(3,k),C3_0(3,k)],'k','LineWidth',2); %C1-C3
-%    hold on 
-%    plot3([C2_0(1,k),C3_0(1,k)],[C2_0(2,k),C3_0(2,k)],[C2_0(3,k),C3_0(3,k)],'k','LineWidth',2); %C2-C3
-%    hold on 
+   % A层平面
+   plot3([A1_0(1,k),A2_0(1,k)],[A1_0(2,k),A2_0(2,k)],[A1_0(3,k),A2_0(3,k)],'k','LineWidth',2); %A1-A2
+   hold on 
+   plot3([A1_0(1,k),A3_0(1,k)],[A1_0(2,k),A3_0(2,k)],[A1_0(3,k),A3_0(3,k)],'k','LineWidth',2); %A1-A3
+   hold on 
+   plot3([A2_0(1,k),A3_0(1,k)],[A2_0(2,k),A3_0(2,k)],[A2_0(3,k),A3_0(3,k)],'k','LineWidth',2); %A2-A3
+   hold on 
+   % B层平面
+   plot3([B1_0(1,k),B2_0(1,k)],[B1_0(2,k),B2_0(2,k)],[B1_0(3,k),B2_0(3,k)],'k','LineWidth',2); %B1-B2
+   hold on 
+   plot3([B1_0(1,k),B3_0(1,k)],[B1_0(2,k),B3_0(2,k)],[B1_0(3,k),B3_0(3,k)],'k','LineWidth',2); %B1-B3
+   hold on 
+   plot3([B2_0(1,k),B3_0(1,k)],[B2_0(2,k),B3_0(2,k)],[B2_0(3,k),B3_0(3,k)],'k','LineWidth',2); %B2-B3
+   hold on 
+   % C层平面
+   plot3([C1_0(1,k),C2_0(1,k)],[C1_0(2,k),C2_0(2,k)],[C1_0(3,k),C2_0(3,k)],'k','LineWidth',2); %C1-C2
+   hold on 
+   plot3([C1_0(1,k),C3_0(1,k)],[C1_0(2,k),C3_0(2,k)],[C1_0(3,k),C3_0(3,k)],'k','LineWidth',2); %C1-C3
+   hold on 
+   plot3([C2_0(1,k),C3_0(1,k)],[C2_0(2,k),C3_0(2,k)],[C2_0(3,k),C3_0(3,k)],'k','LineWidth',2); %C2-C3
+   hold on 
+   % D层平面
+   plot3([D1_0(1,k),D2_0(1,k)],[D1_0(2,k),D2_0(2,k)],[D1_0(3,k),D2_0(3,k)],'k','LineWidth',2); %D1-D2
+   hold on 
+   plot3([D1_0(1,k),D3_0(1,k)],[D1_0(2,k),D3_0(2,k)],[D1_0(3,k),D3_0(3,k)],'k','LineWidth',2); %D1-D3
+   hold on 
+   plot3([D2_0(1,k),D3_0(1,k)],[D2_0(2,k),D3_0(2,k)],[D2_0(3,k),D3_0(3,k)],'k','LineWidth',2); %D2-D3
+   hold on 
+   % E层平面
+   plot3([E1_0(1,k),E2_0(1,k)],[E1_0(2,k),E2_0(2,k)],[E1_0(3,k),E2_0(3,k)],'k','LineWidth',2); %E1-E2
+   hold on 
+   plot3([E1_0(1,k),E3_0(1,k)],[E1_0(2,k),E3_0(2,k)],[E1_0(3,k),E3_0(3,k)],'k','LineWidth',2); %E1-E3
+   hold on 
+   plot3([E2_0(1,k),E3_0(1,k)],[E2_0(2,k),E3_0(2,k)],[E2_0(3,k),E3_0(3,k)],'k','LineWidth',2); %E2-E3
+   hold on 
+   % F层平面
+   plot3([F1_0(1,k),F2_0(1,k)],[F1_0(2,k),F2_0(2,k)],[F1_0(3,k),F2_0(3,k)],'k','LineWidth',2); %F1-F2
+   hold on 
+   plot3([F1_0(1,k),F3_0(1,k)],[F1_0(2,k),F3_0(2,k)],[F1_0(3,k),F3_0(3,k)],'k','LineWidth',2); %F1-F3
+   hold on 
+   plot3([F2_0(1,k),F3_0(1,k)],[F2_0(2,k),F3_0(2,k)],[F2_0(3,k),F3_0(3,k)],'k','LineWidth',2); %F2-F3
+   hold on 
+   % G层平面
+   plot3([G1_0(1,k),G2_0(1,k)],[G1_0(2,k),G2_0(2,k)],[G1_0(3,k),G2_0(3,k)],'k','LineWidth',2); %G1-G2
+   hold on 
+   plot3([G1_0(1,k),G3_0(1,k)],[G1_0(2,k),G3_0(2,k)],[G1_0(3,k),G3_0(3,k)],'k','LineWidth',2); %G1-G3
+   hold on 
+   plot3([G2_0(1,k),G3_0(1,k)],[G2_0(2,k),G3_0(2,k)],[G2_0(3,k),G3_0(3,k)],'k','LineWidth',2); %G2-G3
+   hold on 
    
-%    plot3([A1_0(1,k),B1_0(1,k)],[A1_0(2,k),B1_0(2,k)],[A1_0(3,k),B1_0(3,k)],'k','LineWidth',2); %A1-B1  
-%    hold on  
-%    plot3([A2_0(1,k),B2_0(1,k)],[A2_0(2,k),B2_0(2,k)],[A2_0(3,k),B2_0(3,k)],'k','LineWidth',2); %A2-B2
-%    hold on 
-%    plot3([A3_0(1,k),B3_0(1,k)],[A3_0(2,k),B3_0(2,k)],[A3_0(3,k),B3_0(3,k)],'k','LineWidth',2); %A3-B3
-%    hold on 
-
-%    plot3([B1_0(1,k),C1_0(1,k)],[B1_0(2,k),C1_0(2,k)],[B1_0(3,k),C1_0(3,k)],'k','LineWidth',2); %B1-C1 
-%    hold on  
-%    plot3([B2_0(1,k),C2_0(1,k)],[B2_0(2,k),C2_0(2,k)],[B2_0(3,k),C2_0(3,k)],'k','LineWidth',2); %B2-C2  
-%    hold on 
-%    plot3([B3_0(1,k),C3_0(1,k)],[B3_0(2,k),C3_0(2,k)],[B3_0(3,k),C3_0(3,k)],'k','LineWidth',2); %B3-C3 
-%    hold on
+   % AB层间驱动杆
+   plot3([A1_0(1,k),B1_0(1,k)],[A1_0(2,k),B1_0(2,k)],[A1_0(3,k),B1_0(3,k)],'k','LineWidth',2); %A1-B1  
+   hold on  
+   plot3([A2_0(1,k),B2_0(1,k)],[A2_0(2,k),B2_0(2,k)],[A2_0(3,k),B2_0(3,k)],'k','LineWidth',2); %A2-B2
+   hold on 
+   plot3([A3_0(1,k),B3_0(1,k)],[A3_0(2,k),B3_0(2,k)],[A3_0(3,k),B3_0(3,k)],'k','LineWidth',2); %A3-B3
+   hold on 
+   % BC层间驱动杆
+   plot3([B1_0(1,k),C1_0(1,k)],[B1_0(2,k),C1_0(2,k)],[B1_0(3,k),C1_0(3,k)],'k','LineWidth',2); %B1-C1 
+   hold on  
+   plot3([B2_0(1,k),C2_0(1,k)],[B2_0(2,k),C2_0(2,k)],[B2_0(3,k),C2_0(3,k)],'k','LineWidth',2); %B2-C2  
+   hold on 
+   plot3([B3_0(1,k),C3_0(1,k)],[B3_0(2,k),C3_0(2,k)],[B3_0(3,k),C3_0(3,k)],'k','LineWidth',2); %B3-C3 
+   hold on
+   % CD层间驱动杆
+   plot3([C1_0(1,k),D1_0(1,k)],[C1_0(2,k),D1_0(2,k)],[C1_0(3,k),D1_0(3,k)],'k','LineWidth',2); %C1-D1 
+   hold on  
+   plot3([C2_0(1,k),D2_0(1,k)],[C2_0(2,k),D2_0(2,k)],[C2_0(3,k),D2_0(3,k)],'k','LineWidth',2); %C2-D2  
+   hold on 
+   plot3([C3_0(1,k),D3_0(1,k)],[C3_0(2,k),D3_0(2,k)],[C3_0(3,k),D3_0(3,k)],'k','LineWidth',2); %C3-D3 
+   hold on
+   % DE层间驱动杆
+   plot3([D1_0(1,k),E1_0(1,k)],[D1_0(2,k),E1_0(2,k)],[D1_0(3,k),E1_0(3,k)],'k','LineWidth',2); %D1-E1 
+   hold on  
+   plot3([D2_0(1,k),E2_0(1,k)],[D2_0(2,k),E2_0(2,k)],[D2_0(3,k),E2_0(3,k)],'k','LineWidth',2); %D2-E2  
+   hold on 
+   plot3([D3_0(1,k),E3_0(1,k)],[D3_0(2,k),E3_0(2,k)],[D3_0(3,k),E3_0(3,k)],'k','LineWidth',2); %D3-E3 
+   hold on
+   % EF层间驱动杆
+   plot3([E1_0(1,k),F1_0(1,k)],[E1_0(2,k),F1_0(2,k)],[E1_0(3,k),F1_0(3,k)],'k','LineWidth',2); %E1-F1 
+   hold on  
+   plot3([E2_0(1,k),F2_0(1,k)],[E2_0(2,k),F2_0(2,k)],[E2_0(3,k),F2_0(3,k)],'k','LineWidth',2); %E2-F2  
+   hold on 
+   plot3([E3_0(1,k),F3_0(1,k)],[E3_0(2,k),F3_0(2,k)],[E3_0(3,k),F3_0(3,k)],'k','LineWidth',2); %E3-F3 
+   hold on 
+   % FG层间驱动杆
+   plot3([F1_0(1,k),G1_0(1,k)],[F1_0(2,k),G1_0(2,k)],[F1_0(3,k),G1_0(3,k)],'k','LineWidth',2); %F1-G1 
+   hold on  
+   plot3([F2_0(1,k),G2_0(1,k)],[F2_0(2,k),G2_0(2,k)],[F2_0(3,k),G2_0(3,k)],'k','LineWidth',2); %F2-G2  
+   hold on 
+   plot3([F3_0(1,k),G3_0(1,k)],[F3_0(2,k),G3_0(2,k)],[F3_0(3,k),G3_0(3,k)],'k','LineWidth',2); %F3-G3 
+   hold on 
    
-%    plot3([A0_0(1,k),B0_0(1,k)],[A0_0(2,k),B0_0(2,k)],[A0_0(3,k),B0_0(3,k)],'r','LineWidth',2); %A0-B0
-%    hold on
-%    plot3([B0_0(1,k),C0_0(1,k)],[B0_0(2,k),C0_0(2,k)],[B0_0(3,k),C0_0(3,k)],'r','LineWidth',2); %B0-c0
-%    hold on
+   %中心杆   
+   plot3([A0_0(1,k),B0_0(1,k)],[A0_0(2,k),B0_0(2,k)],[A0_0(3,k),B0_0(3,k)],'r','LineWidth',2); %A0-B0
+   hold on
+   plot3([B0_0(1,k),C0_0(1,k)],[B0_0(2,k),C0_0(2,k)],[B0_0(3,k),C0_0(3,k)],'r','LineWidth',2); %B0-c0
+   hold on
+   plot3([C0_0(1,k),D0_0(1,k)],[C0_0(2,k),D0_0(2,k)],[C0_0(3,k),D0_0(3,k)],'r','LineWidth',2); %C0-D0
+   hold on
+   plot3([D0_0(1,k),E0_0(1,k)],[D0_0(2,k),E0_0(2,k)],[D0_0(3,k),E0_0(3,k)],'r','LineWidth',2); %D0-E0
+   hold on
+   plot3([E0_0(1,k),F0_0(1,k)],[E0_0(2,k),F0_0(2,k)],[E0_0(3,k),F0_0(3,k)],'r','LineWidth',2); %E0-F0
+   hold on
+   plot3([F0_0(1,k),G0_0(1,k)],[F0_0(2,k),G0_0(2,k)],[F0_0(3,k),G0_0(3,k)],'r','LineWidth',2); %F0-G0
+   hold on
    
-%    % 直线末端轨迹
-%    plot3(Path_actual(1,1:k),Path_actual(2,1:k),Path_actual(3,1:k),'g','LineWidth',2); 
-%    hold on
-%    % 期望末端轨迹
-%    plot3(Path_desired(1,1:k),Path_desired(2,1:k),Path_desired(3,1:k),'b','LineWidth',2);
-%    hold on
+   % 直线末端轨迹
+   plot3(Path_actual(1,1:k),Path_actual(2,1:k),Path_actual(3,1:k),'g','LineWidth',2); 
+   hold on
+   % 期望末端轨迹
+   plot3(Path_desired(1,1:k),Path_desired(2,1:k),Path_desired(3,1:k),'b','LineWidth',2);
+   hold on
    
-%    %当前末端点
-%    scatter3(Path_actual(1,1:k),Path_actual(2,1:k),Path_actual(3,1:k),20,'r','filled');
-%    hold on
+   %当前末端点
+   scatter3(Path_actual(1,k),Path_actual(2,k),Path_actual(3,k),20,'y','filled');
+   hold on
 
-%    grid on
+   grid on
    
-%    axis([-1,1,-1,1,0,1]);
-%    % 定义坐标范围
+   axis([-1,1,-1,1,0,1]);
+   % 定义坐标范围
 
-%    % 调用getframe函数生成每个帧
-%    frame = getframe(gcf);
-%    writeVideo(V_Sim,frame);  
-%    hold off
-% end
+   % 调用getframe函数生成每个帧
+   frame = getframe(gcf);
+   writeVideo(V_Sim,frame);  
+   hold off
+end
 
-% close(V_Sim);
+close(V_Sim);
 
 
 
