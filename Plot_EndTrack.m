cb910c1d6659eaaccc992e5450257a9850e4b2db
Prologue <tjuzx2016@126.com>
2020-09-28 21:59:21 +0800

    add设定方向灵敏度分布计算程序

--- Plot_EndTrack.m
+++ Plot_EndTrack.m
@@ -1,16 +1,16 @@
-function [] = Plot_EndTrack(Path_actual,Path_desired)
+function [] = Plot_EndTrack(Path_Actual,Path_Desired)
 
 % 绘制预期末端与实际末端轨迹plot图像
 
 % 直线末端轨迹
-% plot3(Path_actual(1,25000:43890),Path_actual(2,25000:43890),Path_actual(3,25000:43890),'LineWidth',2); 
-plot3(Path_actual(1,:),Path_actual(2,:),Path_actual(3,:),'LineWidth',2); 
+% plot3(Path_Actual(1,25000:43890),Path_Actual(2,25000:43890),Path_Actual(3,25000:43890),'LineWidth',2); 
+plot3(Path_Actual(1,:),Path_Actual(2,:),Path_Actual(3,:),'LineWidth',2); 
 hold on;
 % 期望末端轨迹
-plot3(Path_desired(1,:),Path_desired(2,:),Path_desired(3,:),'LineWidth',2);
+plot3(Path_Desired(1,:),Path_Desired(2,:),Path_Desired(3,:),'LineWidth',2);
 hold on;
 % 起点
-plot3(Path_actual(1,1),Path_actual(2,1),Path_actual(3,1),'.','Color','g','MarkerSize',30);
+plot3(Path_Actual(1,1),Path_Actual(2,1),Path_Actual(3,1),'.','Color','g','MarkerSize',30);
 hold on;
 % 给定坐标范围
 axis([0.3 0.5 -0.4 -0.25 0.86 0.90]);
